438cef95851645f0991a3463f291fa1a9f5ccca1
Pessidjo-Germann <127901437+Pessidjo-Germann@users.noreply.github.com>
2025-06-20 20:54:26 +0100

    Add simplified models for users, profiles, jobs

--- talent_access/users/models.py
+++ talent_access/users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Utilisateur(AbstractUser):
+    class Statut(models.TextChoices):
+        DIPLOME = 'DIPLOME', 'Diplômé'
+        PME = 'PME', 'PME'
+
+    email = models.EmailField(unique=True)
+    date_inscription = models.DateTimeField(auto_now_add=True)
+    statut = models.CharField(max_length=20, choices=Statut.choices)
+
+    def __str__(self):
+        return self.username
