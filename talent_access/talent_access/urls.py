99f67dbbb4980f220686a17b3440701a1a322629
Pessidjo-Germann <127901437+Pessidjo-Germann@users.noreply.github.com>
2025-06-20 21:04:13 +0100

    Add basic homepage with templates

--- talent_access/talent_access/urls.py
+++ talent_access/talent_access/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views as user_views
 
 urlpatterns = [
+    path('', user_views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
